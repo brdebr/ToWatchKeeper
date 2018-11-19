91206743a9a8068a0b3365f99c61347b07fd6ed3
Bryan de Oliveira <brdebr.93@gmail.com>
2018-11-19 19:38:43 +0100

    Add full Content API
    Use this command to easier scaffold
    $ php artisan make:controller ContentController -r --api
    Also use
    `Route::apiResource('contents', 'ContentController');`
    For creating default endpoints

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -24,16 +24,6 @@ class ContentController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -53,45 +43,60 @@ class ContentController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Content  $content
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Content $content)
+    public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Content  $content
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Content $content)
-    {
-        //
+        $content = Content::find($id);
+        $response = array(
+            'status' => 'OK',
+            'message' => 'Here is the content you wanted !',
+            'data' => $content,
+        );
+        return response()->json($response, 200, [], JSON_NUMERIC_CHECK);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Content  $content
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Content $content)
+    public function update(Request $request, $id)
     {
-        //
+        $contentOld = Content::find($id);
+        $result = Content::find($id)->update((json_decode($request->getContent(), true)));
+        $contentNew = Content::find($id);
+        // TODO: Catch errors
+        $response = array(
+            'status' => 'OK',
+            'message' => 'Here is your updated content!',
+            'data' => array(
+               'old' => $contentOld,
+               'new' => $contentNew,
+            )
+        );
+        return response()->json($response, 200, [], JSON_NUMERIC_CHECK);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Content  $content
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Content $content)
+    public function destroy($id)
     {
-        //
+        $content = Content::find($id);
+        $result = Content::find($id)->delete;
+        // TODO: Catch errors
+        $response = array(
+            'status' => 'OK',
+            'message' => 'Here is the content that you deleted!',
+            'data' => $content
+        );
+        return response()->json($response, 200, [], JSON_NUMERIC_CHECK);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/contents', 'ContentController@index')->name('Content.all');
-
-Route::post('/content', 'ContentController@store')->name('Content.store');
+Route::apiResource('contents', 'ContentController');
